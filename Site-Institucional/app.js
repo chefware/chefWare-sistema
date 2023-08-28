08b1c8cbb8fe4035375aab43265d60388402f6b4
ribgu <gustavo.ribeiro@sptech.school>
2023-08-28 19:47:26 -0300

    terminando header site estático

--- Site-Institucional/app.js
+++ Site-Institucional/app.js
@@ -2,5 +2,4 @@ const express = require('express');
 const app = express();
 const link = 'http://localhost:3000';
 app.use(express.static('public'));
-
 app.listen(3000, () => console.log(`Servidor rodando em ${link}`));
