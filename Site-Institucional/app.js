ffdf29b8f67adf62a8827a7c50eef1a761148886
ribgu <gustavo.ribeiro@sptech.school>
2023-08-28 14:25:15 -0300

    Criando estrutura de pastas

---
+++ Site-Institucional/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const link = 'http://localhost:3000';
+app.use(express.static('public'));
+
+app.listen(3000, () => console.log(`Servidor rodando em ${link}`));
