1cadfd4c180452e209f4c132d0cdaf565ac37f71
juliaRufino <julia.rufino@etec.sp.gov.br>
2023-10-14 14:09:40 -0300

    Adicionando outros graficos e as diferentes telas

---
+++ Site-Institucional/public/assets/js/controleTelasDashboard.js
@@ -0,0 +1,11 @@
+const botaoDisco = document.getElementById('botaoDisco');
+
+botaoDisco.addEventListener('click', function() {
+    window.location.href = '../dashboard/dashboard-disco.html';
+});
+
+const botaoCpu = document.getElementById('botaoCpu');
+
+botaoCpu.addEventListener('click', function() {
+    window.location.href = '../dashboard/dashboard-cpu.html';
+});
