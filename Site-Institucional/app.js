17d37566e6380aebaffaa6ee4cd2d324c7180d4d
ribgu <gustavo.ribeiro@sptech.school>
2023-11-11 17:34:54 -0300

    Add routes for empresas and maquinas controllers

--- Site-Institucional/app.js
+++ Site-Institucional/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import funcionariosController from './backend/Funcionarios/funcionariosController.js';
+import empresasController from './backend/Empresas/empresasController.js';
+import maquinasController from './backend/Maquinas/maquinasController.js';
 
 const app = express();
 const link = 'http://localhost:3000';
@@ -8,6 +10,8 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/funcionarios', funcionariosController);
+app.use('/empresas', empresasController);
+app.use('/maquinas', maquinasController);
 
 app.listen(3000, () => console.log(`Servidor rodando em ${link}`));
 
