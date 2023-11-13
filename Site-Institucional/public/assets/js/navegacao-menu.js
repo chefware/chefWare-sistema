41d22163925c26ff3a8b2266263571c848106db3
juliaRufino <julia.rufino@sptech.school>
2023-11-13 19:34:03 -0300

    arrumando a navegação do menu nas páginas

---
+++ Site-Institucional/public/assets/js/navegacao-menu.js
@@ -0,0 +1,17 @@
+const itemMenuMaquina = document.getElementById('itemMenuMaquina');
+
+itemMenuMaquina.addEventListener('click', function () {
+    window.location.href = 'lista-maquinas.html';
+});
+
+const itemMenuEmpresa = document.getElementById('itemMenuEmpresa');
+
+itemMenuEmpresa.addEventListener('click', function () {
+    window.location.href = 'lista-empresas.html';
+});
+
+const itemMenuFuncionario = document.getElementById('itemMenuFuncionario');
+
+itemMenuFuncionario.addEventListener('click', function () {
+    window.location.href = 'lista-funcionario.html';
+});
