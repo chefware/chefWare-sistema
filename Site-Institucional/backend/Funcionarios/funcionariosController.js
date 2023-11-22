ba9fb29bd01ce0b37ab4ee353c3c14ce381065ad
ribgu <gustavo.ribeiro@sptech.school>
2023-11-22 14:55:12 -0300

    editar funcionario com imagem

--- Site-Institucional/backend/Funcionarios/funcionariosController.js
+++ Site-Institucional/backend/Funcionarios/funcionariosController.js
@@ -109,7 +109,7 @@ funcionariosController.post('/', upload.single('foto'), async (req, res) => {
     }
 });
 
-funcionariosController.patch('/:id', upload.single('foto'), upload.single('foto'), async (req, res) => {
+funcionariosController.patch('/:id', upload.single('foto'), async (req, res) => {
     try {
         const { id } = req.params;
         const { nome, email, senha, cpf, cargo, privilegio } = req.body;
@@ -118,29 +118,21 @@ funcionariosController.patch('/:id', upload.single('foto'), upload.single('foto'
 
         if (fotoPerfil) {
             fotoPerfilBuffer = fotoPerfil.buffer;
-            updateData.foto = fotoPerfilBuffer;
-        }
-
-        let updateData = {
-            nome,
-            email,
-            senha,
-            cpf,
-            cargo,
-            privilegio,
-            foto: fotoPerfilBuffer,
-        };
-
-        if (fotoPerfil) {
-            fotoPerfilBuffer = fotoPerfil.buffer;
-            updateData.foto = fotoPerfilBuffer;
         }
 
         const funcionarioAtualizado = await prisma.Funcionario.update({
             where: {
                 id: Number(id),
             },
-            data: updateData,
+            data: {
+                nome,
+                email,
+                senha,
+                cpf,
+                cargo,
+                privilegio,
+                foto: fotoPerfilBuffer,
+            },
         });
 
         res.status(200).json(funcionarioAtualizado);
